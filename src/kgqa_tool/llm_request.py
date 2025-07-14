ae1abc267362959f6b1b570af7609f5afc127afe
Nikit Srivastava <nikit91@gmail.com>
2025-07-14 17:05:50 +0200

    updating function description

--- src/kgqa_tool/llm_request.py
+++ src/kgqa_tool/llm_request.py
@@ -1,1 +1,8 @@
 # Function to prioritize and filter nodes to visit
+def filter_priotize_triples(triple_list, template_file_path):
+    # Go through batches of triple_list and answer the questions in the template file
+    
+    # Merge all the responses from the batches while keeping a priority queue for the nodes to expand
+    
+    # return the findings
+    pass
