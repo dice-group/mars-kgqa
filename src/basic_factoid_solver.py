c2288d19d03fef379cf60c063f17107cfab1174e
Nikit Srivastava <nikit91@gmail.com>
2025-07-16 22:16:16 +0200

    moving llm_request function

--- src/basic_factoid_solver.py
+++ src/basic_factoid_solver.py
@@ -1,6 +1,7 @@
 from src.kgqa_tool.entity_retrieval import find_entities_and_relations
 from src.kgqa_tool.graph_traversal import find_1_hop_triples
-from src.util.llm import get_embeddings, prompt_chat_llm
+from src.kgqa_tool.llm_request import check_if_answer
+from src.util.llm import get_embeddings
 from src.const.misc import WIKIDATA_ENDPOINT_URL
 from src.util.common import dot
 import heapq
@@ -104,41 +105,3 @@ def process_input_query(question_txt, model_config):
       
     # Repeat until the answer is found
     return answer_triple
-
-def check_if_answer(question_txt, top_triples, model_config):
-    # Check if the triple contains the answer to the question
-
-    triples_list = '\n'.join([triple_data.get_verbalization() for triple_data in top_triples])
-
-    check_prompt = f"""Look at the following triples and then either use the provided Answer Format if you find one triple that directly answers the question or use Next Triple Format if no answer is found. Do not write anything else, use only single format.
-
-    Question: {question_txt}
-
-    Triples list:
-    {triples_list}
-
-    ---
-
-    Answer Format:
-
-    Answer: <place the answer triple here>
-
-    ---
-
-    Next Triple Format:
-
-    Next Triple Index: <place the index of the next triple to consider here, use 0-indexing>
-
-    """
-    llm_resp_text = prompt_chat_llm(check_prompt, None, model_config.get_static_instance(), model_config.model_id)
-
-    # Parse the LLM response
-    if "Answer:" in llm_resp_text:
-        answer_triple = llm_resp_text.split("Answer:")[1].strip()
-        return answer_triple, None
-    elif "Next Triple Index:" in llm_resp_text:
-        next_triple_index = int(llm_resp_text.split("Next Triple Index:")[1].strip())
-        return None, next_triple_index
-    else:
-        # If the response doesn't match either format, return None for both
-        return None, None
--- src/kgqa_tool/llm_request.py
+++ src/kgqa_tool/llm_request.py
@@ -1,10 +1,41 @@
 
-from src.util.common import read_dataset, create_directory_if_not_exists, send_to_llm
-# Function to prioritize and filter nodes to visit
-def filter_priotize_triples(triple_list, template_file_path):
-    # TODO: Go through batches of triple_list and answer the questions in the template file
-    
-    # Merge all the responses from the batches while keeping a priority queue for the nodes to expand
-    
-    # return the findings
-    raise NotImplementedError()
+from src.util.llm import prompt_chat_llm
+
+
+def check_if_answer(question_txt, top_triples, model_config):
+    # Check if the triple contains the answer to the question
+
+    triples_list = '\n'.join([triple_data.get_verbalization() for triple_data in top_triples])
+
+    check_prompt = f"""Look at the following triples and then either use the provided Answer Format if you find one triple that directly answers the question or use Next Triple Format if no answer is found. Do not write anything else, use only single format.
+
+    Question: {question_txt}
+
+    Triples list:
+    {triples_list}
+
+    ---
+
+    Answer Format:
+
+    Answer: <place the answer triple here>
+
+    ---
+
+    Next Triple Format:
+
+    Next Triple Index: <place the index of the next triple to consider here, use 0-indexing>
+
+    """
+    llm_resp_text = prompt_chat_llm(check_prompt, None, model_config.get_static_instance(), model_config.model_id)
+
+    # Parse the LLM response
+    if "Answer:" in llm_resp_text:
+        answer_triple = llm_resp_text.split("Answer:")[1].strip()
+        return answer_triple, None
+    elif "Next Triple Index:" in llm_resp_text:
+        next_triple_index = int(llm_resp_text.split("Next Triple Index:")[1].strip())
+        return None, next_triple_index
+    else:
+        # If the response doesn't match either format, return None for both
+        return None, None
