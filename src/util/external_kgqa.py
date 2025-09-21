7bfe87106203d75e64418d1a931d421b9c262976
Nikit Srivastava <nikit91@gmail.com>
2025-09-21 23:34:57 +0200

    adding gerbil eval for external systems

--- src/util/external_kgqa.py
+++ src/util/external_kgqa.py
@@ -9,6 +9,9 @@ from typing import Pattern
 from src.kgqa_tool.llm_request import sparql_filter
 from src.util.process_flow_logger import ProcessFlowLogger
 from src.const.llm import ChatModel
+from src.util.qald_io import convert_basic_output
+from src.util.gerbil import create_export_gerbil_experiment
+from src.const.misc import GERBIL_EXPERIMENT_URI_STORE_FILEPATH
 
 _LANG_FILTER_RE: Pattern = re.compile(
     r"""
@@ -109,9 +112,6 @@ def grasp_output_to_tsv(grasp_out: str, tsv_out: str, refine_sparql: bool, model
         proc_logger.write_log()
         proc_logger.close()
 
-def evaluate_external_system(system_name, kgqa_dataset, split, external_qald_file, lang):
-    pass
-
 def refine_output_sparql(sparql_str, model_config, proc_logger):
     lang_filter = False
     # NOTE: More refinement cases to be added here if needed
@@ -123,18 +123,44 @@ def refine_output_sparql(sparql_str, model_config, proc_logger):
     if to_filter:
         sparql_str = sparql_filter(sparql_str, lang_filter, model_config, proc_logger)
     return sparql_str
+
+def evaluate_external_system(system_name, kgqa_dataset, split, external_tsv_file, gerbil_output_dir, lang):
+    # convert to json file
+    wd_ep = kgqa_dataset.value.preferred_wd_endpoint
+    qald_file_path = kgqa_dataset.value.split_dict[split]
+    json_output_path = external_tsv_file.replace('/tsv/', '/json/').replace('.tsv', '.json')
+    
+    convert_basic_output(external_tsv_file, qald_file_path, json_output_path, False, wd_ep)
+    # upload gerbil experiment
+    gold_dataset_label = f'{kgqa_dataset.value.dataset_id}_{split.name.lower()}'
+    
+    gerbil_result_path = os.path.join(gerbil_output_dir, f'{system_name}__{gold_dataset_label}.csv')
+    
+    create_export_gerbil_experiment(gold_dataset_label, qald_file_path, system_name, json_output_path, lang, gerbil_result_path, GERBIL_EXPERIMENT_URI_STORE_FILEPATH)
             
 # Example usage
 if __name__ == "__main__":
+    from src.const.dataset import KgqaDataset, DatasetSplit
+    
+    ds_obj = KgqaDataset.QALD10
+    ds_split = DatasetSplit.TEST
+    
+    llm_config = ChatModel.GPTOSS120B.value # LLM to use
+    
+    output_dir = 'data_dir/external_systems/grasp/output'
+    
     ## Convert input QALD to jsonl for GRASP
-    # from src.const.dataset import KgqaDataset, DatasetSplit
-    
-    # ds_obj = KgqaDataset.QALD10
-    # ds_split = DatasetSplit.TEST
-    
     # ds_path = ds_obj.value.split_dict[ds_split]
     # qald_to_grasp_jsonl(ds_path, f'data_dir/external_systems/grasp/input/{ds_obj.value.dataset_id}_{ds_split.name.lower()}.jsonl')
     
     ## Convert output of GRASP to TSV to be processed
-    llm_config = ChatModel.GPTOSS120B.value # LLM to use
-    grasp_output_to_tsv('data_dir/external_systems/grasp/output/original/gpt-oss-120b/qald10_test_output.jsonl', 'data_dir/external_systems/grasp/output/tsv/gpt-oss-120b/qald10_test_output.tsv', True, llm_config)
+    tsv_file_path = f'{output_dir}/tsv/gpt-oss-120b/qald10_test_output.tsv'
+    # grasp_output_to_tsv(f'{output_dir}/original/gpt-oss-120b/qald10_test_output.jsonl', tsv_file_path, True, llm_config)
+    
+    ## Generate QALD json and execute gerbil experiment
+    sysname = f'grasp_{llm_config.model_id.lower()}'
+    gerbil_output_dir = f'{output_dir}/gerbil/'
+    
+    evaluate_external_system(sysname, ds_obj, ds_split, tsv_file_path, gerbil_output_dir, 'en')
+    
+    
