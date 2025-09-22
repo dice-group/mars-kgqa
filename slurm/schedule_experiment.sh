298078c3cbf6020e8b8583250af4cedab6c71a06
Nikit Srivastava <nikit91@gmail.com>
2025-09-22 23:18:11 +0200

    updating slurm script

--- slurm/schedule_experiment.sh
+++ slurm/schedule_experiment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Sample usage: bash slurm/schedule_experiment.sh --approach PBSG_MHOP --dataset QALD10_UPDATED_TENTRISQ10 --split TEST --llm GPTOSS120B --topn-count 20 --mhop-limit 5 --include-pattern-count
+
 PASSED_ARGS="$@"
 
 CUR_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -25,4 +27,4 @@ mkdir $CLUSTER_LOG_DIR
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 RUN_NAME="${APPROACH}-${DATASET}-${SPLIT}-${LLM}-${USE_GOLD}-${TIMESTAMP}"
 
-sbatch --job-name=$RUN_NAME --gres=gpu:h100:1 --time=05:00:00 -o "$CLUSTER_LOG_DIR/%x__slurm-%j.out" $CUR_SCRIPT_DIR/../execute_experiment.sh $PASSED_ARGS
+sbatch --job-name=$RUN_NAME --gres=gpu:h100:1 --time=10:00:00 -o "$CLUSTER_LOG_DIR/%x__slurm-%j.out" $CUR_SCRIPT_DIR/../execute_experiment.sh $PASSED_ARGS
