628237dbc0b70fb9a6b4701aa8a10c00f126f158
Nikit Srivastava <nikit91@gmail.com>
2025-09-24 20:34:45 +0200

    minor changes

--- src/const/dataset.py
+++ src/const/dataset.py
@@ -50,7 +50,7 @@ class KgqaDataset(Enum):
     ## Working datasets - Start
     
     QALD9PLUS_UPDATED_TENTRISQ10 =  KgqaDatasetInfo("qald9plus_tentrisq10", "QALD-9-plus (Updated on QALD 10 Wikidata - Tentris)", {
-        DatasetSplit.TRAIN: "data_dir/processed_kgqa_ds/qald9plus/train/tentrisq10_updt_aug_gold.json",
+        DatasetSplit.TRAIN: "data_dir/processed_kgqa_ds/qald9plus/train/tentrisq10_aug_gold.json",
         DatasetSplit.TEST: "data_dir/processed_kgqa_ds/qald9plus/test/tentrisq10_aug_gold.json"
     }, QALD10_TENTRIS_WIKIDATA_EP)
     
